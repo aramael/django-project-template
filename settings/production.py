a930e8eed68b0bd09450c5f346497a4e3bd90c01
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2014-04-25 02:23:06 -0400

    Added SSL Settings to Production Server

--- settings/production.py
+++ settings/production.py
@@ -23,3 +23,23 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 # heroku config:add SECRET_KEY=''
 # </code>
 SECRET_KEY = os.environ['SECRET_KEY']
+
+
+#==============================================================================
+# SSL Processing
+#==============================================================================
+
+# If this is set to True, the cookie will be marked as 'secure,' which means
+# browsers may ensure that the cookie is only sent under an HTTPS connection.
+SESSION_COOKIE_SECURE = False
+
+# If this is set to True, the cookie will be marked as 'secure' which means
+# browsers may ensure that the cookie is only sent under an HTTPS connection.
+CSRF_COOKIE_SECURE = False
+
+# See https://docs.djangoproject.com/en/1.6/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Whether to expire the session when the user closes his or her browser. See
+# https://docs.djangoproject.com/en/1.6/topics/http/sessions/#browser-length-vs-persistent-sessions
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
